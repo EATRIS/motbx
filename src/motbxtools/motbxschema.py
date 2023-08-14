09095ab1ba64c2c43da4541af640cb23d8c59579
niehues <1405356+niehues@users.noreply.github.com>
2023-08-14 16:31:04 +0200

    code cleaning

--- src/motbxtools/motbxschema.py
+++ src/motbxtools/motbxschema.py
@@ -8,7 +8,7 @@ import validators
 
 class MotbxSchema():
     """This class stores a JSON Schema as a dictionary.
-    """    
+    """
     def __init__(self, json_path):
         self._json_path = json_path
         self.schema = json.load(open(self._json_path, "r"))
@@ -17,62 +17,62 @@ class MotbxSchema():
 
     def validate(self):
         """Validate schema against metaschema JSON schema draft 2020-12.
-        """        
+        """
         jsonschema.Draft202012Validator.check_schema(self.schema)
         return
 
 
 class MotbxResource():
     """This class stores a MOTBX resource as a dictionary.
-    """    
+    """
     def __init__(self, yaml_path):
         """Initialise MOTBX resource object.
 
         :param yaml_path: Path to YAML file containing a MOTBX resource
         :type yaml_path: str
-        """        
+        """
         self._yaml_path = yaml_path
         self.load()
         return
 
     def load(self):
         """Load a MOTBX resource from a YAML file.
-        """        
+        """
         with open(self._yaml_path, "r") as fp:
             self.resource = yaml.safe_load(fp)
         return
 
     def validate(self, motbx_schema):
         """Validate MOTBX resource with JSON schema and confirm that URL is
-        valid and live. 
+        valid and live.
 
         :param motbx_schema: _description_
         :type motbx_schema: :class:`~MotbxSchema`
         :raises Exception: Validation of URL fails
-        """        
+        """
         # validate against JSON schema
         jsonschema.validate(
-            self.resource, motbx_schema.schema, 
-            format_checker = jsonschema.FormatChecker())
+            self.resource, motbx_schema.schema,
+            format_checker=jsonschema.FormatChecker())
         # validate URL (pattern https://* already validated by jsonschema)
         url = self.resource["resource"]["resourceUrl"]
         try:
-            response = validators.url(url, public = True) 
+            response = validators.url(url, public=True)
             if not response:
                 raise Exception
-        except:
+        except Exception:
             raise
         # check if URL is live
         try:
             response = requests.get(url)
             response.raise_for_status()
-        except:
+        except Exception:
             raise
         return
 
     def flatten(self, fieldnames):
-        """Iterate through nested dictionary (from JSON) and return dictionary 
-        with single level for writing CSV file. Keep only keys present in 
+        """Iterate through nested dictionary (from JSON) and return dictionary
+        with single level for writing CSV file. Keep only keys present in
         fieldnames.
 
         :param fieldnames: CSV column names (fieldnames) of a
@@ -80,28 +80,28 @@ class MotbxResource():
         :type fieldnames: list
         :return: A row for :func:`~csv.DictWriter.writerow`
         :rtype: dict
-        """        
-        def _flatten(resource, fieldnames, row = {}):
+        """
+        def _flatten(resource, fieldnames, row={}):
             """Recursive function to retrieve all required fields provided in
             fieldnames from the (nested) resource dictionary.
 
             :param resource: MOTBX resource or part of the resource
             :type resource: dict
-            :param fieldnames: CSV column names (fieldnames) of a 
+            :param fieldnames: CSV column names (fieldnames) of a
                 :class:`~csv.DictWriter` instance
             :type fieldnames: list
-            :param row: An incomplete row for :func:`~csv.DictWriter.writerow`, 
+            :param row: An incomplete row for :func:`~csv.DictWriter.writerow`,
                 defaults to {}
             :type row: dict, optional
-            """            
+            """
             for k, v in resource.items():
-                if type(v) == type(str()) and k in fieldnames:
+                if isinstance(v, str) and k in fieldnames:
                     row[k] = v
-                elif type(v) == type(list()) and k in fieldnames:
+                elif isinstance(v, list) and k in fieldnames:
                     row[k] = ", ".join(v)
-                elif type(v) == type(dict()):
-                    _flatten(v, fieldnames, row = row)
-            return(row)
-        
+                elif isinstance(v, dict):
+                    _flatten(v, fieldnames, row=row)
+            return row
+
         row = _flatten(self.resource, fieldnames)
         return row
--- test/test_motbxschema.py
+++ test/test_motbxschema.py
@@ -2,29 +2,29 @@
 import unittest
 import os
 import sys
+import jsonschema
+import requests
 module_path = os.path.abspath(os.path.join("src"))
 if module_path not in sys.path:
     sys.path.append(module_path)
 from motbxtools import motbxschema
-import jsonschema
-import requests
 
 
 class TestMotbxSchema(unittest.TestCase):
     """Test JSON schema that defines structure of MOTBX resources.
-    """    
+    """
     def test_validate(self):
         try:
             schema = motbxschema.MotbxSchema("schema/motbxschema.json")
             schema.validate()
-        except:
+        except Exception:
             self.fail("MotbxSchema() could not be instantiated/validated.")
 
 
 class TestMotbxResourceMethods(unittest.TestCase):
     """Test that validation of MOTBX resources succeeds for given examples and
     fails under certain conditions.
-    """    
+    """
     def setUp(self):
         self.schema = motbxschema.MotbxSchema("schema/motbxschema.json")
         self.resources_pass = "test/resources_pass"
@@ -32,23 +32,23 @@ class TestMotbxResourceMethods(unittest.TestCase):
 
     def test_validate_pass(self):
         """Validation of these resources should succeed
-        """        
+        """
         # iterate through resources
         for root, dirs, files in os.walk(self.resources_pass):
-            for name in files: 
+            for name in files:
                 if not name.endswith(".yaml"):
                     continue
                 # load test resource and validate
                 resource = motbxschema.MotbxResource(os.path.join(root, name))
                 try:
                     resource.validate(self.schema)
-                except:
+                except Exception:
                     self.fail("MotbxResource.validate() failed")
-        
+
     def test_validate_fail(self):
         """These MOTBX resources do not meet the requirements. Test that their
         validation fails.
-        """        
+        """
         with self.assertRaises(jsonschema.exceptions.ValidationError):
             resource = motbxschema.MotbxResource(
                 os.path.join(self.resources_fail, "testfailDate.yaml"))
@@ -73,7 +73,7 @@ class TestMotbxResourceMethods(unittest.TestCase):
             resource = motbxschema.MotbxResource(
                 os.path.join(self.resources_fail, "testfailUrl3.yaml"))
             resource.validate(self.schema)
-        
+
 
 if __name__ == '__main__':
     unittest.main()
