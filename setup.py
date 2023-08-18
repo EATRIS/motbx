1e155e693133e740ab36e9fafed3caee766a27c7
niehues <1405356+niehues@users.noreply.github.com>
2023-08-18 16:06:00 +0200

    fixed missing line

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    [...]
+    name='motbxtools',
+    version='0.1.0',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
