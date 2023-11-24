d05b73ec7a819b8c89170bb8c568f166807a7021
niehues <1405356+niehues@users.noreply.github.com>
2023-11-24 15:20:47 +0100

    test input parameter version

--- src/motbxtools/utils/summarise_resources.py
+++ src/motbxtools/utils/summarise_resources.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import sys
 from pathlib import Path
 from motbxtools import motbxschema
 
@@ -7,6 +8,12 @@ MOTBX_DIR = Path.cwd()
 
 
 if __name__ == "__main__":
+    version = sys.argv[1]
+    if version == "latest":
+        with open(MOTBX_DIR.joinpath("resources/MOTBX_version.yaml"), "r") as f:
+            MOTBX_VERSION = yaml.safe_load(f)
+
     print(MOTBX_DIR)
     with open(MOTBX_DIR.joinpath("resources/summary/test.txt"), "w") as f:
         print(MOTBX_DIR, file=f)
+        print(MOTBX_VERSION['latest'])
